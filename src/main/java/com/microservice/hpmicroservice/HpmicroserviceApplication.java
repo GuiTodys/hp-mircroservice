9d45436b82bd96202d19b6f903d881dfe6704524
GuiTodys <guilhermetodys123@hotmail.com>
2022-04-19 22:32:05 -0300

    Project created

---
+++ src/main/java/com/microservice/hpmicroservice/HpmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.hpmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/hpmicroservice/HpmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.hpmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
