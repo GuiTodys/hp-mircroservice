5bd49c7e72a627f369a4aa027caec3607c5a342c
GuiTodys <guilhermetodys123@hotmail.com>
2022-05-26 23:07:02 -0300

    Project uploaded

--- src/main/java/com/microservice/hpmicroservice/controllers/AlunoController.java
+++ src/main/java/com/microservice/hpmicroservice/controllers/AlunoController.java
@@ -10,6 +10,7 @@ import com.microservice.hpmicroservice.payloads.AlunoResponseWithHouse;
 import com.microservice.hpmicroservice.services.aluno.CadastroAluno;
 import com.microservice.hpmicroservice.services.aluno.ConsultarAluno;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,6 +31,7 @@ public class AlunoController {
     }
 
     @GetMapping("/{alunoId}")
+    @Cacheable(cacheNames = "info", key = "#alunoId")
     public AlunoResponseWithHouse getAlunoInformation(
             @PathVariable Long alunoId
     ) {
