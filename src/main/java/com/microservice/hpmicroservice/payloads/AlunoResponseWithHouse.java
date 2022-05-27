48be8e74eef716b0a05dd805f8117062ee2b77c4
GuiTodys <guilhermetodys123@hotmail.com>
2022-05-26 23:00:45 -0300

    Project uploaded

--- src/main/java/com/microservice/hpmicroservice/payloads/AlunoResponseWithHouse.java
+++ src/main/java/com/microservice/hpmicroservice/payloads/AlunoResponseWithHouse.java
@@ -2,8 +2,10 @@ package com.microservice.hpmicroservice.payloads;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class AlunoResponseWithHouse {
+public class AlunoResponseWithHouse implements Serializable {
     private Long id;
     private String name;
     private String serie;
--- src/main/java/com/microservice/hpmicroservice/services/aluno/ConsultarInformacoesCasa.java
+++ src/main/java/com/microservice/hpmicroservice/services/aluno/ConsultarInformacoesCasa.java
@@ -1,13 +1,16 @@
 package com.microservice.hpmicroservice.services.aluno;
 
 import com.microservice.hpmicroservice.payloads.HouseDetails;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Log4j2
 public class ConsultarInformacoesCasa {
     public HouseDetails execute(String casaId) {
+        log.info("Searching house name...");
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
